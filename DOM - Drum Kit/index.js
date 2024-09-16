bccbdcc9c13b51d6cea8f2fdc1c7b3832833557d
Raphaelj <raphaeljames897@gmail.com>
2024-09-16 12:06:14 +0100

    minor changes

--- DOM - Drum Kit/index.js
+++ DOM - Drum Kit/index.js
@@ -1,10 +1,23 @@
 const numberOfDrums = document.querySelectorAll('.drum').length;
 
 for (let i = 0; i < numberOfDrums; i++) {
-	document.querySelectorAll('.drum')[i].addEventListener('click', handleClick, false);
+	document.querySelectorAll('.drum')[i].addEventListener('click', () => handleClick(), false);
 }
 
 function handleClick() {
-	alert('I got clicked!');
+    alert('');
 }
 
+// audio.play();
+// setTimeout(() => audio.pause(), 10000);
+// const audio = new Audio('./sounds/Goodbye.mp3');
+
+const drumKeys = {
+    w: document.querySelector(".drum.w"),
+    a: document.querySelector(".drum.a"),
+    s: document.querySelector(".drum.s"),
+    d: document.querySelector(".drum.d"),
+    j: document.querySelector(".drum.j"),
+    k: document.querySelector(".drum.k"),
+    l: document.querySelector(".drum.l"),
+}
